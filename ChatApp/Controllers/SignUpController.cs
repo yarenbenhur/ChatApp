81b0997d1911457bc8b16a77d61e397124609928
yarenbenhur <yarenbenhur2001@gmail.com>
2022-07-06 11:06:39 +0300

    Sign Up Controller

---
+++ ChatApp/Controllers/SignUpController.cs
@@ -0,0 +1,52 @@
+﻿using ChatApp_DataAccess;
+using ChatApp_Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SignUpController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            User newUser = new();
+            return View(newUser);
+        }
+        [HttpPost]
+        public IActionResult Index(User newUser)
+        {
+            try
+            {
+                _db.Users.Add(new User
+                {
+                    UserName = newUser.UserName,
+                    FirstName = newUser.FirstName,
+                    LastName = newUser.LastName,
+                    Password = newUser.Password
+                });
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            catch(Exception)
+            {
+                ModelState.AddModelError("", "Error is occured while creating new account!");
+            }
+           
+
+
+            return View();
+        }
+
+    }
+}
